2f581da19f8d82c97e7ec1aed893c8e68eb932c8
jalkestrup <jalkestrup@gmail.com>
2023-01-13 11:59:07 +0100

    updated dvc to Jesper & docker build file

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,10 +2,11 @@ import os
 import pickle
 import torch
 import pytorch_lightning as pl
+
 # TODO: get paths right, the train_subset.yaml paths have too many ../../ >.<
 print(os.getcwd())
 print(os.path.dirname(os.path.abspath(__file__)))
-#from src.models.model import LightningBert
+# from src.models.model import LightningBert
 from model import LightningBert
 from pytorch_lightning import Trainer
 import hydra
@@ -13,29 +14,29 @@ import wandb
 from omegaconf import OmegaConf
 
 if torch.has_cuda:
-    acc="cuda"
+    acc = "cuda"
 elif torch.has_mps:
-    acc="mps"
+    acc = "mps"
 else:
-    acc=None
+    acc = None
 print(f"Using {acc} accelerator")
 
 
 @hydra.main(config_path="../../config", config_name="default_config.yaml")
 def train(cfg):
-    
+
     cfg_dict = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
-    with open('../../../src/deployment/latest_training_dict.pickle', 'wb') as handle:
+    with open("../../../src/deployment/latest_training_dict.pickle", "wb") as handle:
         pickle.dump(cfg_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     model = LightningBert(cfg_dict)
 
-    trainer = Trainer(max_epochs=cfg.train['n_epochs'],
-        accelerator=acc)
+    trainer = Trainer(max_epochs=cfg.train["n_epochs"], accelerator=acc)
     trainer.fit(model)
 
     # Save
     torch.save(model.state_dict(), cfg.train["modelpath"])
+
 
 if __name__ == "__main__":
     train()
