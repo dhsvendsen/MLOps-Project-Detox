f50971a67a84e1a9ac7ce6a139378f74db0e963c
jalkestrup <jalkestrup@gmail.com>
2023-01-19 16:46:49 +0100

    fixed wandb 8-)

--- src/models/sweep_model.py
+++ src/models/sweep_model.py
@@ -8,11 +8,17 @@ import yaml
 
 from pytorch_lightning.loggers import WandbLogger
 
-wandb.init(config={"learning_rate": 0.01, "batch_size": 32, "epochs": 5}, project="gpt-4", entity="oldboys")
+sweep_configuration = {
+    "method": "random",
+    "name": "sweep",
+    "metric": {"goal": "minimize", "name": "val_loss"},
+    "parameters": {
+        "batch_size": {"values": [16, 32, 64]},
+        "epochs": {"values": [5, 10, 15]},
+        "lr": {"max": 0.1, "min": 0.0001},
+    },
+}
 
-
-# Initialize a WandbLogger
-wandb_logger = WandbLogger(project="gpt-4")
 
 if torch.has_cuda:
     device = "cuda"
@@ -23,35 +29,31 @@ else:
 print(f"Using {device} accelerator")
 
 
-sweep_configuration = {
-    'method': 'random',
-    'name': 'sweep',
-    'metric': {
-        'goal': 'minimize', 
-        'name': 'val_loss'
-		},
-    'parameters': {
-        'batch_size': {'values': [16, 32, 64]},
-        'epochs': {'values': [5, 10, 15]},
-        'lr': {'max': 0.1, 'min': 0.0001}
-     }
-}
-
 print("Starting sweep")
 sweep_id = wandb.sweep(sweep_configuration, project="gpt-4", entity="oldboys")
 
+
 def train():
+    wandb.init()
     lr = wandb.config.lr
     batch_size = wandb.config.batch_size
-    
+    print(f"lr: {lr}, batch_size: {batch_size}")
 
     # Load config
     with open("config/config.json") as file:
         cfg = json.load(file)
-    
+
+    cfg["model"]["lr"] = lr
+    cfg["model"]["batch_size"] = batch_size
+    print(cfg)
+    # Initialize a WandbLogger
+    wandb_logger = WandbLogger(project="gpt-4")
+
     model = LightningBertBinary(cfg)
     wandb_logger.watch(model)
-    trainer = Trainer(max_epochs=cfg["model"]["n_epochs"], accelerator=device, logger = wandb_logger)
+    trainer = Trainer(
+        max_epochs=cfg["model"]["n_epochs"], accelerator=device, logger=wandb_logger
+    )
     trainer.fit(model)
 
     # Save
@@ -59,9 +61,7 @@ def train():
     torch.save(model.state_dict(), cfg["paths"]["path_checkpoint"])
 
 
-
 if __name__ == "__main__":
-    #train()
+    # train()
     #
-    wandb.agent(sweep_id, train)
-    
+    wandb.agent(sweep_id, function=train, count=4)
